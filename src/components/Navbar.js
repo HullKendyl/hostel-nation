d5a8e0062a9e60f5f1a5a4ccc38bb8f6346cfa1a
Kendyl Hull <hullkendyl@gmail.com>
2022-01-24 22:27:52 -0500

    added logo and navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import pic from "../images/hostel-nation-transparent.png";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={pic} />
+        </nav>   
+    )
+}
